3a98dc939ca4cf5840eb97fb9eff8f4acac0fb71
Timofey Stolbov <timofey.stolbov@zoho.eu>
2022-04-04 16:23:44 +0500

    Update posinstall.sh
    Have your comments :)

--- posinstall.sh
+++ posinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Здесь должно быть set -ex как минимум.
 
 if [[ $EUID -ne 0 ]]; then
  echo "This script must be run as root" 
@@ -6,7 +7,7 @@ if [[ $EUID -ne 0 ]]; then
 else
  #Update and Upgrade
  echo "Updating and Upgrading"
- apt-get update && sudo apt-get upgrade -y
+ apt-get update && sudo apt-get upgrade -y  # не нужно здесь sudo
 
 sudo apt-get install dialog
  cmd=(dialog --separate-output --checklist "Please Select Software you want to install:" 22 76 16)
@@ -30,17 +31,17 @@ sudo apt-get install dialog
  2)
  #Install Python 3.10 with pip
  echo "installing Python 3.10 with pip"
- sudo add-apt-repository ppa:deadsnakes/ppa -y
- sudo apt -y install python3.10 
- sudo apt -y install python3-pip
- sudo apt-get install python3-distutils -y
+ sudo add-apt-repository ppa:deadsnakes/ppa -y   # не нужно здесь sudo
+ sudo apt -y install python3.10   # не нужно здесь sudo
+ sudo apt -y install python3-pip  # не нужно здесь sudo
+ sudo apt-get install python3-distutils -y  # не нужно здесь sudo
  python3 -m pip install --upgrade pip
  ;;
  3)
  #Install Docker and docker-compose
  echo "installing Docker"
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
- add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -  # не нужно здесь sudo
+ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" # имя релиза лучше не хардкодить
  apt update
  apt install -y docker-ce
  usermod -aG docker ${USER}
@@ -57,20 +58,17 @@ sudo apt-get install dialog
  #Install Node.js, npm, yarn
  echo "Installing Node.js, npm, yarn"
  curl -fsSL https://deb.nodesource.com/setup_14.x | bash
- apt install -y nodejs=14.19.1-deb-1nodesource1
+ apt install -y nodejs=14.19.1-deb-1nodesource1  # точно-точно нужно настолько версии прибивать?
  npm install -g npm@6.14.13
  npm install -g yarn@1.22.15
- source ~/.bashrc
+ source ~/.bashrc  # зачем это?
  chown -R ${USER} "/usr/lib/node_modules"
  chmod -R 777 "${HOME}/.npm"
  npm config set prefix "${HOME}/.npm"
- export PATH="${PATH}:${HOME}/.npm/bin"
+ export PATH="${PATH}:${HOME}/.npm/bin" # перестанет же работать в следующей оболчке
  ;;
 
 
  esac
  done
 fi
-
-
-
